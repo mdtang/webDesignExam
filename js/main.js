9238f2085c5bf9aea02c0447865b8cb2b57a8d46
Mingda Tang <mdtang@umich.edu>
2018-04-16 14:02:02 -0400

    finished step 7

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $('#hide-button').on('click', function() {
     $('#image1').hide();
+    $('#image2').addClass('center-image');
   });
   $('#show-button').on('click', function() {
     $('#image1').show();
+    $('#image2').removeClass('center-image');
   });
 });
