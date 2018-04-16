c75d5cfb907db8cc484792fcb7524eb850d29fd8
Mingda Tang <mdtang@umich.edu>
2018-04-16 13:54:28 -0400

    finished step 5 and step 6

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#hide-button').on('click', function() {
+    $('#image1').hide();
+  });
+  $('#show-button').on('click', function() {
+    $('#image1').show();
+  });
+});
